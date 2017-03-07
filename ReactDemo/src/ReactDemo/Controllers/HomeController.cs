2fde5bad632a35cc6772d8c9615989b1bec0be18
Simon Martin (simon.martin@canterbury.ac.uk) <simon.martin@canterbury.ac.uk>
2017-03-07 09:33:02 +0000

    Server-side data
    The data is now coming from a data model and controller. Plus a new route that returns JJSON

--- ReactDemo/src/ReactDemo/Controllers/HomeController.cs
+++ ReactDemo/src/ReactDemo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ReactDemo.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,43 @@ namespace ReactDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly IList<CommentModel> comments;
+
+        static HomeController()
+        {
+            comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Simon Martin",
+                    Text = "Something I've said"
+                },
+                new CommentModel
+                {
+                    Id=2,
+                    Author ="Someone Else",
+                    Text="What they said"
+                },
+                new CommentModel
+                {
+                    Id=3,
+                    Author = "Mr Happy",
+                    Text="This is *another* comment."
+                },
+            };
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("comments")]
+        [ResponseCache(Location =ResponseCacheLocation.None, NoStore =true)]
+        public ActionResult Comments()
+        {
+            return Json(comments);
         }
     }
 }
